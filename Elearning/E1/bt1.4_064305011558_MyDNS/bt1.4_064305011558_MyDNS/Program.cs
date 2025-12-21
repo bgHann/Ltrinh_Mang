cb22f698c6a47084859c16a91c7aa424974e9348
Bui Gia Han <hanbuii7705@gmail.com>
2025-12-21 20:11:49 +0700

    bài 1.4 ử dụng thư viện có sẵn - Chương trình DNS Lookup

---
+++ Elearning/E1/bt1.4_064305011558_MyDNS/bt1.4_064305011558_MyDNS/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net; 
+
+namespace bt1._4_064305011558_MyDNS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+			Console.InputEncoding = Encoding.UTF8;
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.Title = "064305011558 - Bùi Gia Hân";
+			string domain; // biến lưu tên truy vấn
+			/// ===== KIỂM TRA THAM SỐ DÒNG LỆNH =====
+			// Nếu khi chạy chương trình có truyền tham số
+			// ví dụ: MyDNS.exe google.com
+
+			if (args.Length > 0)
+			{
+				domain = args[0]; // Lấy tên miền từ dòng lệnh
+			}
+			else
+			{
+				// Nếu KHÔNG có tham số → nhập từ bàn phím
+				Console.Write("Nhập tên miền: ");
+				domain = Console.ReadLine();
+			}
+
+			// truy vấn DNS
+			try
+			{
+				// Gửi yêu cầu DNS để lấy thông tin tên miền
+				IPHostEntry entry = Dns.GetHostEntry(domain);
+				// in ra màn hình truy vans
+				Console.WriteLine("Host name : " + entry.HostName);
+				Console.WriteLine("Danh sách địa chỉ IP : ");
+				foreach (IPAddress ip in entry.AddressList)
+				{
+					Console.WriteLine("- " + ip.ToString());
+				}
+			}
+			catch (Exception ex)//  // Nếu có lỗi (nhập sai tên miền, mất mạng...)
+			{
+				Console.WriteLine("Lỗi truy vấn DNS: " + ex.Message);
+			}
+
+			Console.ReadLine();
+		}
+    }
+}
---
+++ Elearning/E1/bt1.4_064305011558_MyDNS/bt1.4_064305011558_MyDNS/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bt1.4_064305011558_MyDNS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bt1.4_064305011558_MyDNS")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a20ccf7-fe2b-4d26-9e9b-cc61fa766dd1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
